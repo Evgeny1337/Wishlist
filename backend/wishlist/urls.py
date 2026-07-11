899da38a0e325253c562e2f73a299d5158a96f74
Evgeny1337 <genya-sozykin@mail.ru>
2026-07-11 19:26:10 +0500

    Тест инвайта

--- backend/wishlist/urls.py
+++ backend/wishlist/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .api import api
+from invites.views import InviteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls ),
+    path('invite/', InviteView.as_view(), name='invite'),
 ]
